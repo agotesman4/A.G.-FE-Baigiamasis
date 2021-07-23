778b244484abb9c19326ca53255e46e4aa2d6293
agotesman4 <adomasgo@gmail.com>
2021-07-23 09:02:19 +0300

    nav + header almost done need some testing/fixing

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -16,7 +16,7 @@ export default {
 header {
   z-index: 99;
   flex-direction: row;
-  background-color: #17263e;
+  background-color: #291136;
 
   @media (min-width: 900px) {
     min-height: 100%;
